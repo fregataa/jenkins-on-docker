b2f7c69e79907135ff83af19f5e68c5d98f2e97d
Sang Hun <oper6909@gmail.com>
2020-04-29 02:48:23 +0900

    blah blah

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World! blah blah");
+        System.out.println("Hello World! ffff");
     }
 }
