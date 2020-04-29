61a35eac77253ed8f046d7b35a1b3e1b17185dc6
Sang Hun <oper6909@gmail.com>
2020-04-30 03:53:45 +0900

    slack test3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello Slack ! ver2");
+        System.out.println("Hello Slack ! ver3");
     }
 }
